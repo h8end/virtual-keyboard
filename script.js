8ac67f84f4cb5e9f8db162f49e649be2b200f680
h8end <h8end@mail.ru>
2023-05-02 02:03:32 +0300

    fix: keyboard layout changed

--- script.js
+++ script.js
@@ -3,19 +3,13 @@ const keyboard = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 8,
     97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 13, 122, 
     120, 99, 118, 98, 110, 109, 44, 46, 47, 32,];
 
-
-// document.keypress = function (event) {
-//     keyboard.push(event.charCode);
-//     console.log(keyboard);
-// };
-
 function init() {
     let out = '';
     for (let i = 0; i < keyboard.length; i++) {
         if (i == 14 || i == 27 || i == 39) {
             out += '<div class = "clearfix"></div>';
         }
-        out += '<div class="k-key" data = "' + keyboard[i] + '" >' +  String.fromCharCode(keyboard[i]) + '</div>';
+        out += '<div class="k-key" data = "' + keyboard[i] + '" >' + String.fromCharCode(keyboard[i]) + '</div>';
     }
     document.querySelector('body').innerHTML = out;
 }
